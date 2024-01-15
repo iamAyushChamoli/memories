9f384cbcf85ff80fd20045a564cddab033e5cdf2
unknown <ayushcodingprofile@gmail.com>
2024-01-15 21:06:59 +0530

    Cloud DB and Routing

--- server/index.js
+++ server/index.js
@@ -3,13 +3,24 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
 
 const app=express();
+
+//ROUTING
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({limit: "30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
 app.use(cors());
 
 //DB MongoDB Atlas
+const CONNECTION_URL ='mongodb+srv://ayush:root@cluster0.7kcuofk.mongodb.net/';
+const PORT = process.env.PORT || 5000;
 
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then (()=>app.listen(PORT, ()=> console.log(`Server Running on port: ${PORT}`)))
+.catch((error)=>console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
 
